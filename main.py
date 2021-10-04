905d5995d145d18626ac932f4564ed7621110f22
7oakswelcomesrefugees <7oakswelcomesrefugees@gmail.com>
2021-10-04 19:04:14 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  3 01:44:06 2021
+
+@author: samwestlake
+"""
+
+import requests
+
+page = requests.get("https://www.bbcgoodfood.com/recipes/spicy-peanut-chicken-pies")
+print("hello")
